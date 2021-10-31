477cb4f23f60f89fffa88117e95cef5555f59853
Cédric <cedricme4@gmail.com>
2021-10-31 11:37:15 +0100

    Ajout de l'interface client, Ajout d'un logo temporaire CFun

--- pSportPPE/src/main/java/com/pSportPPE/pSportPPE/FXMLDocumentController.java
+++ pSportPPE/src/main/java/com/pSportPPE/pSportPPE/FXMLDocumentController.java
@@ -9,15 +9,24 @@ import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 import java.sql.ResultSet;
 
 
+/**
+ * @author cedric
+ *
+ */
 public class FXMLDocumentController implements Initializable {
 	
 	@FXML
@@ -50,20 +59,32 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private ComboBox<?> type_up;
     
+  
     Connection conn = null;
     ResultSet rs = null;
     PreparedStatement pst = null;
     
+   
+    /**
+     *  Fonction permettant de switch vers la page connexion
+     */
     public void LoginpaneShow () {
     	pane_login.setVisible(true);
     	pane_signup.setVisible(false);
     }
     
+    /**
+     *  Fonction permettant de switch vers la page inscription
+     */
     public void SignuppaneShow () {
     	pane_login.setVisible(false);
     	pane_signup.setVisible(true);
     }
 	
+    /**
+     * @param event lorsque l'on presse un bouton
+     * @throws Exception 
+     */
     @FXML 
     private void Login (ActionEvent event) throws Exception {
     	conn = SQLdbConnection.ConnectDb();
@@ -76,6 +97,12 @@ public class FXMLDocumentController implements Initializable {
     		rs = pst.executeQuery();
     		if(rs.next()) {
     			 JOptionPane.showMessageDialog(null, "Nom d'utilisateur et mot de passe correct");
+    			 btn_login.getScene().getWindow().hide();
+    			 Parent root = FXMLLoader.load(getClass().getResource("Client.fxml"));
+    			 Stage mainStage = new Stage (); 
+    			 Scene scene = new Scene (root);
+    			 mainStage.setScene(scene);
+    			 mainStage.show();
     		} else 
     		JOptionPane.showMessageDialog(null, "Nom d'utilisateur et/ou mot de passe Incorrect");
     	} catch (Exception e) {
